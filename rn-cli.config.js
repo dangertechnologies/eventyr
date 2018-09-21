8e06a72f38c4fddcdf57a6893b1a30812ea3f75e
Victor Rudolfsson <>
2018-09-21 18:43:51 +0200

    Updated dependencies

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,4 +1,15 @@
+const blacklist = require("metro-config/src/defaults/blacklist");
+
 module.exports = {
+  watchFolders: alternateRoots,
+  resolver: {
+    blacklistRE: blacklist
+  },
+
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-typescript-transformer")
+  },
+
   getTransformModulePath() {
     return require.resolve("react-native-typescript-transformer");
   },
