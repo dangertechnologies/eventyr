cee8e6f3a735157e090b1bce07522ebf1d34f9ee
Victor Rudolfsson <>
2018-09-21 19:44:44 +0200

    Fixed Xcode 10 configuration

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,14 +1,4 @@
-const blacklist = require("metro-config/src/defaults/blacklist");
-
 module.exports = {
-  resolver: {
-    blacklistRE: blacklist
-  },
-
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-typescript-transformer")
-  },
-
   getTransformModulePath() {
     return require.resolve("react-native-typescript-transformer");
   },
